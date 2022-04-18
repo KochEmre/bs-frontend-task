124ecf97fbde2ae771cf100c1f8c799b346d8d67
KochEmre <kochhhemre@gmail.com>
2022-04-18 15:59:14 +0300

    backup

--- src/components/portfolioViewer/index.jsx
+++ src/components/portfolioViewer/index.jsx
@@ -5,16 +5,17 @@ import { ReactComponent as ListView } from '../../assets/icons/list-view.svg';
 
 const PortFolioViewer = () => {
     const [viewType, setViewType] = useState('grid');
+    const [typeFilter, setTypeFilter] = useState('All');
 
     return (
         <div className="portfolio-container">
             <div className="filter-viewtype-container">
                 <div className="filter-container">
-                    <span>All</span>
-                    <span>Print</span>
-                    <span>Photography</span>
-                    <span>Web</span>
-                    <span>Applications</span>
+                    <span onClick={() => setTypeFilter('All')} className={`${typeFilter === "All" ? "active" : ""}`}>All</span>
+                    <span onClick={() => setTypeFilter('Print')} className={`${typeFilter === "Print" ? "active" : ""}`}>Print</span>
+                    <span onClick={() => setTypeFilter('Photography')} className={`${typeFilter === "Photography" ? "active" : ""}`}>Photography</span>
+                    <span onClick={() => setTypeFilter('Web')} className={`${typeFilter === "Web" ? "active" : ""}`}>Web</span>
+                    <span onClick={() => setTypeFilter('Applications')} className={`${typeFilter === "Applications" ? "active" : ""}`}>Applications</span>
                 </div>
                 <div className="viewtype-container">
                     <GridView title='Grid View' onClick={() => setViewType('grid')} className={`${viewType === "grid" ? "active" : ""}`} />
