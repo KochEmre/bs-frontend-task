4bc77c29950796a0e73b26a8c817423f30825677
KochEmre <kochhhemre@gmail.com>
2022-04-18 19:03:01 +0300

    Stated creating About Page - backup

---
+++ src/components/services/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./services.scss"
+
+const Services = () => {
+    return (
+        <div>
+            <div className="tab-background">
+            </div>
+            <div className="service-tab-container">
+                <div className="service-tab-item">websites</div>
+                <div className="service-tab-item">photography</div>
+                <div className="service-tab-item">seo</div>
+                <div className="service-tab-item">applications</div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Services
--- src/pages/aboutPage/index.jsx
+++ src/pages/aboutPage/index.jsx
@@ -1,13 +1,73 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PageTitle from '../../components/pageTitle'
+import Services from '../../components/services'
+import AboutImage from "./../../assets/images/about.png"
 import "./aboutPage.scss"
 
 const AboutPage = () => {
   return (
-    <div>
-      <PageTitle title="ABOUT MY BUSINESS"/>
+    <Fragment>
+      <PageTitle title="ABOUT MY BUSINESS" />
+      <div className="content-container">
+        <div className="flex-container">
+          <img src={AboutImage} alt="About" width={380} height={260} />
+          <div className="column-container">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque eu erat lacus, vel congue mauris. Fusce velit
+              justo, faucibus eu sagittis ac, gravida quis tortor. Suspendisse
+              non urna mi, quis tincidunt eros. Nullam tellus turpis,
+              fringilla sit amet congue ut, luctus a nulla. Donec sit amet
+              sapien neque, id ullamcorper diam. Nulla facilisi. Pellentesque
+              pellentesque arcu a elit congue lacinia.
+            </p>
+            <p>
+              Nullam tellus turpis, fringilla sit amet congue ut, luctus a
+              nulla. Donec sit amet sapien neque, id ullamcorper diam. Nulla
+              facilisi. Pellentesque pellentesque arcu a elit congue lacinia.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque eu erat lacus, vel congue mauris. Fusce velit
+              justo, faucibus eu sagittis ac, gravida quis tortor. Suspendisse
+              non urna mi, quis tincidunt eros. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit.
+            </p>
+          </div>
 
-    </div>
+        </div>
+
+        <div className="flex-container">
+          <div>
+            <h3>Mission Statement</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque eu erat lacus, vel congue mauris. Fusce velit
+              justo, faucibus eu sagittis ac, gravida quis tortor. Suspendisse
+              non urna mi, quis tincidunt eros. Nullam tellus turpis,
+              fringilla sit amet congue ut, luctus a nulla. Donec sit amet
+              sapien neque, id ullamcorper diam. Nulla facilisi. Pellentesque
+              pellentesque arcu a elit congue lacinia.
+            </p>
+          </div>
+          <div>
+            <h3>Fun FActs</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Pellentesque eu erat lacus, vel congue mauris. Fusce velit
+              justo, faucibus eu sagittis ac, gravida quis tortor. Suspendisse
+              non urna mi, quis tincidunt eros. Nullam tellus turpis,
+              fringilla sit amet congue ut, luctus a nulla. Donec sit amet
+              sapien neque, id ullamcorper diam. Nulla facilisi. Pellentesque
+              pellentesque arcu a elit congue lacinia.
+            </p>
+          </div>
+        </div>
+        <div className="service-container">
+          <h3>Services</h3>
+          <Services/>
+        </div>
+       
+      </div>
+    </Fragment>
   )
 }
 
